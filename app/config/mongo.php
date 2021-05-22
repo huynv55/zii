057f23632d5a4f064d5e4a49bf0cced4385e22f3
huynv_55 <huynv55@gmail.com>
2021-05-22 21:11:21 +0700

    install guzzlehttp/guzzle package

--- app/config/mongo.php
+++ app/config/mongo.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'host'      => getenv("MONGO_HOST"),
-    'port'		=> getenv("MONGO_PORT"),
+    'port'      => getenv("MONGO_PORT"),
     'user'      => getenv("MONGO_USER"),
     'password'  => getenv("MONGO_PASSWORD"),
     'authDB'    => ''
--- app/services/HttpClient.php
+++ app/services/HttpClient.php
@@ -1,68 +1,43 @@
 <?php
 
 class HttpClient {
-
+	const BASE_URL = '';
 	const CURL_TIMEOUT = 360;
 
+	public static function client($headers = []) {
+		$client = new \GuzzleHttp\Client([
+			// Base URI is used with relative requests
+			'base_uri' => self::BASE_URL,
+			// You can set any number of default request options.\
+			'timeout'  => self::CURL_TIMEOUT,
+			'headers' => $headers
+		]);
+		return $client;
+	}
+
 	public static function get($url, $query_data = null, $headers = null) {
-		return self::request('GET', $url, $query_data, $headers);
+		return self::client()->request('GET', $url, ['query' => $query_data, 'headers' => $headers]);
 	}
 
 	public static function post($url, $post_fields = null, $headers = null) {
-		return self::request('POST', $url, $post_fields, $headers);
+		return self::client()->request('POST', $url, ['form_params' => $post_fields, 'headers' => $headers]);
 	}
 
 	public static function put($url, $put_fields = null, $headers = null) {
-		return self::request('PUT', $url, $put_fields, $headers);
+		return self::client()->request('PUT', $url, ['form_params' => $post_fields, 'headers' => $headers]);
 	}
 
 	public static function delete($url, $delete_fields = null, $headers = null) {
-		return self::request('DELETE', $url, $delete_fields, $headers);
+		return self::client()->request('DELETE', $url, ['form_params' => $delete_fields, 'headers' => $headers]);
 	}
 
-	public static function upload($url, $field, $path_file, $data = null, $headers = null) {
-		if ($data && !empty($data)) {
-			$data[$field] = curl_file_create($path_file);
-		} else {
-			$data = [];
-			$data[$field] = curl_file_create($path_file);
-		}
-		return self::request('POST', $url, $data, $headers);
-	}
-
-	public static function requestPostPayload($url, $data = null, $headers = array('Content-Type:application/json')) {
-		$ch = curl_init();
-
-		// don't return headers
-		curl_setopt($ch, CURLOPT_HEADER, false);
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		if ($data && !empty($data)) {
-			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-		}
-		if ($headers && !empty($headers)) {
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		}
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-		curl_setopt($ch, CURLOPT_TIMEOUT, self::CURL_TIMEOUT);
-		$body = curl_exec($ch);
-		$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		if (curl_errno($ch)) {
-			$msg = 'Error:' . curl_error($ch);
-			trigger_error( $msg, E_USER_WARNING);
-			return null;
-		}
-		curl_close($ch);
-		return compact('status', 'body');
+	public static function requestPostPayload($url, $data = null, $headers = []) {
+		return self::client()->request('POST', $url, ['json' => $data, 'headers' => $headers]);
 	}
 
 
 	public static function request($method, $url, $data = null, $headers = null) {
 		$ch = curl_init();
-
 		// don't return headers
 		curl_setopt($ch, CURLOPT_HEADER, false);
 
