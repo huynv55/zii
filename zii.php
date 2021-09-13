cfbce77ce2d3616a7138f5e8646fc6eb2e8c2d2f
huynv_55 <huynv55@gmail.com>
2021-09-13 14:50:44 +0700

    update zii.php

--- zii.php
+++ zii.php
@@ -3,15 +3,9 @@ require realpath(__DIR__.'/vendor/autoload.php');
 define("ACL_USER_CRYPTO", false);
 define("ROOT_PATH", realpath(__DIR__.'/').DIRECTORY_SEPARATOR);
 define("APP_PATH", realpath(__DIR__.'/app/').DIRECTORY_SEPARATOR);
-/**
- * begin load env file
- * link doc : https://github.com/vlucas/phpdotenv
- */
-$dotenv = Dotenv\Dotenv::createImmutable(ROOT_PATH);
-$dotenv->load();
-/**
- * end load env file
- */
+require realpath(__DIR__.'/helpers/helper.php');
+
+(new DotEnv(ROOT_PATH . '.env'))->load();
 
 $argvs = $_SERVER['argv'];
 $action = !empty($argvs[1]) ? $argvs[1] : 'init';
